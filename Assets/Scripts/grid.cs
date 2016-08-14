7f88d18dbc6dd7fb4b13d1fbc9edc6ce17fb90a7
Scott Bromander <scott@primeacademy.io>
2016-08-13 21:39:06 -0500

    Piece prefab

--- Assets/Scripts/grid.cs
+++ Assets/Scripts/grid.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class grid : MonoBehaviour {
+
+	public enum PieceType {
+		NORMAL, 
+		COUNT
+	}
+
+	public int xDim;
+	public int yDim;
+
+	[System.Serializable]
+	public struct PiecePrefab {
+		public PieceType type;
+		public GameObject prefab;
+	}
+
+	public PiecePrefab[] piecePrefabs;
+
+	private Dictionary<PieceType, GameObject> piecePrefabDict;
 
 	// Use this for initialization
 	void Start () {

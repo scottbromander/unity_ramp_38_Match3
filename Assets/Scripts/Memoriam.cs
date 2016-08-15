ebe01a1fbce6a8a6432bd3e1578e95665ef77c45
Scott Bromander <scott@primeacademy.io>
2016-08-14 20:27:46 -0500

    in memoriam

---
+++ Assets/Scripts/Memoriam.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Memoriam : MonoBehaviour {
+
+	//In loving memory of Bailey, rest in peace.
+}
--- Assets/Scripts/grid.cs
+++ Assets/Scripts/grid.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class grid : MonoBehaviour {
+public class Grid : MonoBehaviour {
 
 	public enum PieceType {
 		NORMAL, 

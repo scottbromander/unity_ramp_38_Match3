05a6485717b41975e4d6b2d6699b63d160a4f22a
Scott Bromander <scott@primeacademy.io>
2016-08-15 20:18:06 -0500

    piece creation

--- Assets/Scripts/grid.cs
+++ Assets/Scripts/grid.cs
@@ -19,12 +19,26 @@ public class Grid : MonoBehaviour {
 	}
 
 	public PiecePrefab[] piecePrefabs;
+	public GameObject backgroundPrefab;
 
 	private Dictionary<PieceType, GameObject> piecePrefabDict;
 
 	// Use this for initialization
 	void Start () {
-	
+		piecePrefabDict = new Dictionary<PieceType, GameObject> ();
+
+		for (int i = 0; i < piecePrefabs.Length; i++) {
+			if(!piecePrefabDict.ContainsKey(piecePrefabs[i].type)){
+				piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab);
+			}
+		}
+
+		for (int x = 0; x < xDim; x++) {
+			for (int y = 0; y < yDim; y++) {
+				GameObject background = (GameObject)Instantiate (backgroundPrefab, new Vector3(x, y, 0), Quaternion.identity);
+				background.transform.parent = transform;
+			}
+		}
 	}
 	
 	// Update is called once per frame
